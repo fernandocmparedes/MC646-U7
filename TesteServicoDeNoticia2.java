c30d74d5e0ea327ba27a67e61755617db08ee244
fernandocmparedes <71674660+fernandocmparedes@users.noreply.github.com>
2021-01-04 23:55:18 -0300

    Arquivos da atividade U7

---
+++ TesteServicoDeNoticia2.java
@@ -0,0 +1,178 @@
+package com.redcompany.testes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.redcompany.wm3.Wm3Application;
+import com.redcompany.wm3.domain.AnexoNoticia;
+import com.redcompany.wm3.domain.Categoria;
+import com.redcompany.wm3.domain.Noticia;
+import com.redcompany.wm3.domain.Usuario;
+import com.redcompany.wm3.domain.servico.ServicoNoticia;
+
+@SpringBootTest(classes = Wm3Application.class)
+@RunWith(SpringRunner.class)
+public class TesteServicoDeNoticia {
+
+	@Autowired
+	private ServicoNoticia servicoNoticia;
+	
+	private static String idUsuarioTeste = "5c94ffcde4b03d74e12c56a5";
+	
+	@Test
+	@Order(0)
+	public void buscarNoticias() {
+		List<String> perfis = new ArrayList<String>();
+		perfis.add("ADMINISTRADOR");
+		
+		Assert.assertNotNull(servicoNoticia.obterTodas(perfis));
+	}
+	
+	@Test
+	@Order(1)
+	public void SimularCRUD() {
+		List<String> perfis = new ArrayList<String>();
+		perfis.add("ADMINISTRADOR");
+		
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfis);
+		
+		Noticia noticia = new Noticia();
+		noticia.setTitulo("Teste");
+		noticia.setTexto("Teste");
+		noticia.setResumo("Teste");
+		noticia.setAutor("Teste");
+		noticia.setPublicador(usuario);
+		noticia.setUrlCapa("http://site.imagem.com");
+		noticia.setCategoria(new Categoria("5dbb4f29b90d423c503f0d23", "News"));
+		noticia.setAnexos(new ArrayList<AnexoNoticia>());
+		noticia.setIdGrupos(new ArrayList<String>());
+		
+		
+		//ADICIONAR
+		Noticia noticiaCriada = servicoNoticia.salvar(noticia);
+		//se foia dicionado o id é diferente de null ou de ""
+		noticia = servicoNoticia.buscar(noticiaCriada.get_id());
+		Assert.assertNotNull(noticia.get_id());
+		Assert.assertNotEquals("", noticia.get_id());
+		
+		
+		//ALTERAR
+		noticia.setTitulo("Titulo alterado");
+		servicoNoticia.alterar(noticia.get_id(), noticia);
+		noticia = servicoNoticia.buscar(noticia.get_id());
+		Assert.assertEquals("Titulo alterado", noticia.getTitulo());
+		
+		
+		//REMOVER
+		servicoNoticia.remover2(noticia.get_id());
+		Assert.assertNull(servicoNoticia.buscar2(noticia.get_id()));
+	}
+	
+	@Test
+	@Order(1)
+	public void SimularMutacoes() {
+		List<String> perfis = new ArrayList<String>();
+		perfis.add("ADMINISTRADOR");
+		
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfis);
+		
+		Noticia noticia = new Noticia();
+		noticia.setTitulo("Teste");
+		noticia.setTexto("Teste");
+		noticia.setResumo("Teste");
+		noticia.setAutor("Teste");
+		noticia.setPublicador(usuario);
+		noticia.setUrlCapa("http://site.imagem.com");
+		noticia.setCategoria(new Categoria("5dbb4f29b90d423c503f0d23", "News"));
+		noticia.setAnexos(new ArrayList<AnexoNoticia>());
+		noticia.setIdGrupos(new ArrayList<String>());
+		
+		
+		//ADICIONAR
+		Noticia noticiaCriada = servicoNoticia.salvar(noticia);
+		//se foia dicionado o id é diferente de null ou de ""
+		noticia = servicoNoticia.buscar(noticiaCriada.get_id());
+		Assert.assertNotNull(noticia.get_id());
+		Assert.assertNotEquals("", noticia.get_id());
+		
+		Assert.assertEquals(noticia.get_id(), noticiaCriada.get_id());
+		Assert.assertEquals(noticia.getAutor(), noticiaCriada.getAutor());
+		Assert.assertEquals(noticia.getResumo(), noticiaCriada.getResumo());
+		Assert.assertEquals(noticia.getTexto(), noticiaCriada.getTexto());
+		Assert.assertEquals(noticia.getTitulo(), noticiaCriada.getTitulo());
+		Assert.assertEquals(noticia.getUrlCapa(), noticiaCriada.getUrlCapa());
+		
+		Assert.assertEquals(noticia.getPrioridade(), noticiaCriada.getPrioridade());
+		Assert.assertEquals(noticia.getPublicador().getEmail(), noticiaCriada.getPublicador().getEmail());
+		
+		//ALTERAR
+		noticia.setTitulo("Titulo alterado");
+		servicoNoticia.alterar(noticia.get_id(), noticia);
+		noticia = servicoNoticia.buscar(noticia.get_id());
+		Assert.assertEquals("Titulo alterado", noticia.getTitulo());
+		
+		
+		//REMOVER
+		servicoNoticia.remover2(noticia.get_id());
+		Assert.assertNull(servicoNoticia.buscar2(noticia.get_id()));
+	}
+	
+	/*@Test
+	@Order(2)
+	public void SimularCRUD_com_valores_invalidos() {
+		List<String> perfis = new ArrayList<String>();
+		perfis.add("ADMINISTRADOR");
+		
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingat");
+		usuario.setCodigosPerfil(perfis);
+		
+		Noticia noticia = new Noticia();
+		noticia.setTitulo("");
+		noticia.setTexto("");
+		noticia.setResumo("");
+		noticia.setAutor("");
+		noticia.setPublicador(usuario);
+		noticia.setUrlCapa("");
+		noticia.setCategoria(new Categoria("5dbb4f29b90d423c503f0d23", "News"));
+		noticia.setAnexos(new ArrayList<AnexoNoticia>());
+		noticia.setIdGrupos(new ArrayList<String>());
+		
+		
+		//ADICIONAR
+		noticia = servicoNoticia.salvar(noticia);
+		//se foi adicionado o id é diferente de null ou de ""
+		Assert.assertNull(noticia);
+		
+		if(noticia != null)
+		noticia = servicoNoticia.buscar2(noticia.get_id());
+		
+		
+		//ALTERAR
+		noticia.setTitulo("Titulo alterado");
+		servicoNoticia.alterar(noticia.get_id(), noticia);
+		noticia = servicoNoticia.buscar(noticia.get_id());
+		Assert.assertEquals("Titulo alterado", noticia.getTitulo());
+		
+		
+		//REMOVER
+		servicoNoticia.remover2(noticia.get_id());
+		Assert.assertNull(servicoNoticia.buscar2(noticia.get_id()));
+	}*/
+
+}
---
+++ TesteServicoMensagem2.java
@@ -0,0 +1,219 @@
+package com.redcompany.testes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.Order;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.redcompany.wm3.Wm3Application;
+import com.redcompany.wm3.domain.Mensagem;
+import com.redcompany.wm3.domain.MensagemVO;
+import com.redcompany.wm3.domain.TipoMensagem;
+import com.redcompany.wm3.domain.Usuario;
+import com.redcompany.wm3.domain.servico.ServicoEnvioDeMensagem;
+import com.redcompany.wm3.domain.servico.ServicoMensagem;
+
+@SpringBootTest(classes = Wm3Application.class)
+@RunWith(SpringRunner.class)
+public class TesteServicoMensagem {
+
+	@Autowired
+	private ServicoMensagem servicoMensagem;
+	
+	@Autowired
+	private ServicoEnvioDeMensagem servicoEnvioDeMensagem;
+	
+	private static String idUsuarioTeste = "5c94ffcde4b03d74e12c56a5";
+	private static String idUsuarioTeste2 = "5d25f8f939f8f93804e3cde9";
+	
+	private static String idUsuarioNaoExistente= "5c94ffcde4b03d74e12c56";
+	
+	@Test
+	@Order(3)
+	public void buscarMensagens() {
+		
+		Assert.assertNotEquals(0, servicoMensagem.ultimasMensagens(idUsuarioTeste, 0L).size());
+		
+		//Não pode exixstir mensagens qu vieram do futuro
+		Assert.assertEquals(0, servicoMensagem.ultimasMensagens(idUsuarioTeste, System.currentTimeMillis()+(1000*60*60*24)).size());
+	}
+	
+	@Test
+	@Order(4)
+	public void EnviarMensagens() {
+
+		List<String> perfil = new ArrayList<String>();
+		perfil.add("ADMINISTRADOR");
+		
+		MensagemVO mensagem = new MensagemVO(idUsuarioTeste2, "Mensagem de Teste", TipoMensagem.MENSAGEM);
+		
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfil);
+		
+		Assert.assertNotNull(servicoEnvioDeMensagem.enviar(mensagem, usuario));
+		servicoMensagem.adiocionarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+	}
+	
+	@Test
+	@Order(5)
+	public void Simular() {
+		List<Mensagem> msgs;
+		int qtdMensagensNaoLidas, qtdMensagens;
+		msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioTeste2, 0L);
+		servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		List<String> perfil = new ArrayList<String>();
+		perfil.add("ADMINISTRADOR");
+		
+		Assert.assertNotEquals(0, msgs.size());
+		qtdMensagens = msgs.size();
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(0, qtdMensagensNaoLidas);
+		
+		MensagemVO mensagem = new MensagemVO(idUsuarioTeste2, "Mensagem de Teste", TipoMensagem.MENSAGEM);
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfil);
+		
+		Assert.assertNotNull(servicoEnvioDeMensagem.enviar(mensagem, usuario));	
+		servicoMensagem.adiocionarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(1, qtdMensagensNaoLidas);
+		
+		msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioTeste2, 0L);
+		servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertNotEquals(0, msgs.size());
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(0, qtdMensagensNaoLidas);
+		Assert.assertEquals(qtdMensagens+1, msgs.size());
+		
+		
+	}
+	
+	@Test
+	@Order(6)
+	public void Mutacoes() {
+		List<Mensagem> msgs;
+		int qtdMensagensNaoLidas, qtdMensagens;
+		msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioTeste2, 0L);
+		servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		List<String> perfil = new ArrayList<String>();
+		perfil.add("ADMINISTRADOR");
+		
+		Assert.assertNotEquals(0, msgs.size());
+		qtdMensagens = msgs.size();
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(0, qtdMensagensNaoLidas);
+		
+		MensagemVO mensagem = new MensagemVO(idUsuarioTeste2, "Mensagem de Teste", TipoMensagem.MENSAGEM);
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfil);
+		
+		Mensagem msgSent = servicoEnvioDeMensagem.enviar(mensagem, usuario);
+		Assert.assertNotNull(msgSent);	
+		servicoMensagem.adiocionarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(1, qtdMensagensNaoLidas);
+		
+		msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioTeste2, 0L);
+		Mensagem msgSent2 = msgs.get(msgs.size()-1);
+		
+		System.out.println(msgSent2.get_id() + " = " + msgSent.get_id());
+		
+		Assert.assertEquals(msgSent2.get_id(), msgSent.get_id());
+		Assert.assertEquals(msgSent2.getDestinatario(), msgSent.getDestinatario());
+		Assert.assertEquals(msgSent2.getMensagem(), msgSent.getMensagem());
+		Assert.assertEquals(msgSent2.getNomeDestinatario(), msgSent.getNomeDestinatario());
+		Assert.assertEquals(msgSent2.getNomeRemetente(), msgSent.getNomeRemetente());
+		Assert.assertEquals(msgSent2.getRemetente(), msgSent.getRemetente());
+		Assert.assertEquals(msgSent2.getTimestamp(), msgSent.getTimestamp());
+		Assert.assertEquals(msgSent2.getTipoMensagem(), msgSent.getTipoMensagem());
+		Assert.assertEquals(msgSent2.getIdParaSubstituir(), msgSent.getIdParaSubstituir());
+		Assert.assertEquals(msgSent2.getUrl(), msgSent.getUrl());
+		
+		servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertNotEquals(0, msgs.size());
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioTeste2);
+		
+		Assert.assertEquals(0, qtdMensagensNaoLidas);
+		Assert.assertEquals(qtdMensagens+1, msgs.size());
+		
+		
+	}
+	
+	/*@Test
+	@Order(6)
+	public void SimularDestinatarioNaoExistente() {
+		List<Mensagem> msgs;
+		int qtdMensagensNaoLidas, qtdMensagens;
+		msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioNaoExistente, 0L);
+		servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioNaoExistente);
+		
+		List<String> perfil = new ArrayList<String>();
+		perfil.add("ADMINISTRADOR");
+		
+		Assert.assertEquals(0, msgs.size());
+		qtdMensagens = msgs.size();
+		
+		qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioNaoExistente);
+		
+		Assert.assertEquals(0, qtdMensagensNaoLidas);
+		
+		MensagemVO mensagem = new MensagemVO(idUsuarioNaoExistente, "Mensagem de Teste", TipoMensagem.MENSAGEM);
+		Usuario usuario = new Usuario(idUsuarioTeste);
+		usuario.setNome("Gunter Mingato");
+		usuario.setEmail("guntermingato@gmail.com");
+		usuario.setCodigosPerfil(perfil);
+		Mensagem msg = null;
+		try {
+			msg = servicoEnvioDeMensagem.enviar(mensagem, usuario);
+			
+			servicoMensagem.adiocionarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioNaoExistente);
+			
+			qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioNaoExistente);
+			
+			Assert.assertEquals(0, qtdMensagensNaoLidas);
+			
+			msgs = servicoMensagem.ultimasMensagens(idUsuarioTeste, idUsuarioNaoExistente, 0L);
+			servicoMensagem.zerarNumeroMensagensNaolidas(idUsuarioTeste, idUsuarioNaoExistente);
+			
+			Assert.assertEquals(0, msgs.size());
+			
+			qtdMensagensNaoLidas = servicoMensagem.quantidadeMensagensNaoLidas(idUsuarioTeste, idUsuarioNaoExistente);
+			
+			Assert.assertEquals(0, qtdMensagensNaoLidas);
+			Assert.assertEquals(0, msgs.size());
+			
+		}catch(UsuarioNaoExistenteException e) {
+			msg = null;
+		}
+		Assert.assertNull(msg);
+		
+		
+	}*/
+}
